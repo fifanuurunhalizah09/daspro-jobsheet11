1dcacdfbd839e458913a88705a7074c3b2f4eb62
fifanuurunhalizah09 <eljeaje@gmail.com>
2024-11-21 08:41:00 +0700

    percobaan2

--- Kafe09.java
+++ Kafe09.java
@@ -1,6 +1,11 @@
 public class Kafe09 {
 
-    public static void main(String[] args) {
+    public static void Menu(String namaPelanggan, boolean isMember) {
+        System.out.println("Selamat Datang, " + namaPelanggan + "!");
+
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
         System.out.println("==== MENU RESTO KAFE ====");
         System.out.println("1. Kopi Hitam - RP 15,000");
         System.out.println("2. Cappucino - RP 20,000");
@@ -10,5 +15,8 @@ public class Kafe09 {
         System.out.println("6. Mie Goreng - RP 18,000");
         System.out.println("==========================");
         System.out.println("Silahkan pilih menu yang Anda inginkan.");
+    }
+    public static void main(String[] args) {
+        Menu ("Andi", true);
     }
 }
